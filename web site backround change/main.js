8be5e3c274871cee49c1ce64d8fb664810598b67
Arun-cn <arunchandran4321@gmail.com>
2022-12-03 11:43:23 +0400

    add button

---
+++ web site backround change/main.js
@@ -0,0 +1,7 @@
+ var button= document.getElementsByTagName("button");
+ button.item(0).onclick(change)
+
+ var change = function bgcolor(){
+   var bg = Document.getElementsByTagName("body").item(0)
+
+ }
